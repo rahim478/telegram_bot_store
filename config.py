c314b5b7c4ef10824f760171d647a67a5344bbe7
rahim478 <81584372+rahim478@users.noreply.github.com>
2025-08-19 01:16:22 +0100

    Update config.py
    no webhook

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 BINANCE_ID = os.getenv("BINANCE_ID")
-WEBHOOK_URL = "https://telegram-bot-store-ckhq.onrender.com/YOUR_WEBHOOK_PATH"
+
+
 
