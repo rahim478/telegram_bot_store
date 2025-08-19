7bef2bf2fe49b19a2bf69e821c01af6d43b39cad
rahim478 <81584372+rahim478@users.noreply.github.com>
2025-08-19 01:14:02 +0100

    Update config.py
    syntax err

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 BINANCE_ID = os.getenv("BINANCE_ID")
-https://telegram-bot-store-ckhq.onrender.com
+WEBHOOK_URL = "https://telegram-bot-store-ckhq.onrender.com/YOUR_WEBHOOK_PATH"
+
